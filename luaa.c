398c0ad19c6693f157a03be375f91db5a5125e1d
Timidger <APragmaticPlace@gmail.com>
2019-08-14 19:21:38 -0400

    Organize all xwayland code into x11/

--- luaa.c
+++ luaa.c
@@ -44,7 +44,6 @@
 #include "common/backtrace.h"
 #include "common/version.h"
 #include "config.h"
-#include "event.h"
 #include "objects/client.h"
 #include "objects/drawable.h"
 #include "objects/drawin.h"
@@ -54,12 +53,13 @@
 #include "objects/selection_transfer.h"
 #include "objects/selection_watcher.h"
 #include "objects/tag.h"
-#include "property.h"
-#include "selection.h"
 #include "spawn.h"
 #include "systray.h"
 #include "xkb.h"
 #include "xrdb.h"
+
+#include "x11/property.h"
+#include "x11/selection.h"
 
 #include <lua.h>
 #include <lauxlib.h>
